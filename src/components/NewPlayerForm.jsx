a347ee96e28d5479e091415ee210b4a506863637
Jaron Proehl <jaron@strategicmissionelements.com>
2024-01-16 23:03:34 -0500

    final product

--- src/components/NewPlayerForm.jsx
+++ src/components/NewPlayerForm.jsx
@@ -3,59 +3,85 @@ import { useState } from "react";
 const cohortName = "2309-FTB-ET-WEB-PT";
 export const BaseUrl = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
 
-export default function newPlayerForm() {
+export default function NewPlayerForm() {
+  const initialPlayerState = {
+    name: "",
+    breed: "",
+    status: "",
+    imageUrl: "",
+  };
 
-    const [name, setName] = useState("");
-    const [breed, setBreed] = useState("");
-    const [status, setStatus] = useState ('bench');
-    const [imageUrl, setImageUrl] = useState("");
+  const [player, setPlayer] = useState(initialPlayerState);
 
-async function handleAddPlayer (event) {
-    event.preventDefault ();
+  async function handleAddPlayer(event) {
+    event.preventDefault();
 
     try {
-        const response = await fetch (`${BaseUrl}/players`, {
-            method: "POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify({
-                name,
-                breed,
-                status,
-                imageUrl,
-            }),
-        })
-        const result = await response.json();
-        console.log ('handleAddPlayer', result);
-        window.location.reload();
+      const response = await fetch(`${BaseUrl}/players`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(player),
+      });
+      const result = await response.json();
+      console.log("handleAddPlayer", result);
     } catch (error) {
-        console.error("Cannot add player");
+      console.error("Cannot add player", error);
     }
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setPlayer((prevPlayer) => ({ ...prevPlayer, [name]: value, }));
+  };
+
+  return (
+    <div>
+      <h1>New Player Form</h1>
+      <form>
+        <label>
+          Puppy's Name:
+          <input
+            type="text"
+            name="name"
+            value={player.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Puppy's Breed:
+          <input
+            type="text"
+            name="breed"
+            value={player.breed}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Puppy's Game Day Status:
+          <input
+            type="text"
+            name="status"
+            value={player.status}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Puppy's Picture:
+          <input
+            type="text"
+            name="imageUrl"
+            value={player.imageUrl}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button className="button" onClick={handleAddPlayer}>
+          Add Puppy to the Roster!
+        </button>
+      </form>
+    </div>
+  );
 }
 
-    return (
-    <div>
-        <h1>New Player Form</h1>
-    <form>
-    <label>
-      Puppy's Name:
-      <input type="text" name="name" onChange={(event) => setName(event.target.value)}/>
-    </label>
-    <label>
-      Puppy's Breed:
-      <input type="text" name="breed" onChange={(event) => setBreed(event.target.value)}/>
-    </label>
-    <label>
-      Puppy's Game Day Status:
-      <input type="text" name="status" onChange={(event) => setStatus(event.target.value)}/>
-    </label>
-    <label>
-      Puppy's Picture:
-      <input type="text" name="imageUrl" onChange={(event) => setImageUrl(event.target.value)}/>
-    </label>
-    <button className="button" onClick={handleAddPlayer}>Add Puppy to the Roster!</button>
-  </form>
-      </div>
-    );
-  }
+
